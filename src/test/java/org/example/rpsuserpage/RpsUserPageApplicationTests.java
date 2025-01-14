207091d473676eb2f47886a06587280656995e9d
Pavel <khorn218@gmail.com>
2025-01-14 21:42:44 +0300

    Основа проекта

---
+++ src/test/java/org/example/rpsuserpage/RpsUserPageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.rpsuserpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpsUserPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
